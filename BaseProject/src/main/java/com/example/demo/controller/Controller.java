3a4d652416f9cdc6054313d2cc0201bb47a70ff8
ameyp <ameyp@AmeyGalaxyBook>
2025-10-19 22:34:49 +0530

    with return view

--- BaseProject/src/main/java/com/example/demo/controller/Controller.java
+++ BaseProject/src/main/java/com/example/demo/controller/Controller.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Controller {
@@ -11,4 +12,11 @@ public class Controller {
 		return "Working Throught pipline..";
 	}
 
+
+    @GetMapping("/helloView")
+    public ModelAndView helloView() {
+        ModelAndView mav = new ModelAndView("hello"); // hello.html
+        mav.addObject("name", "Amey");
+        return mav;
+    }
 }
